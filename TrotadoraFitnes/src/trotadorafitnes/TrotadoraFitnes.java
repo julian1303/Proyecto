ac361cc7dcc7aa49b0cd3dcad9e872b5eacedcde
Julian Garcia <j-j1303@hotmail.com>
2015-09-02 11:45:22 -0500

    Impresion de hola mundoooo

--- TrotadoraFitnes/src/trotadorafitnes/TrotadoraFitnes.java
+++ TrotadoraFitnes/src/trotadorafitnes/TrotadoraFitnes.java
@@ -15,7 +15,7 @@ public class TrotadoraFitnes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundoooo");
     }
     
 }
